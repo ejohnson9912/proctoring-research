320057b5da6038135d100225f2da6782a27232e2
ejohnson9912 <39468634+ejohnson9912@users.noreply.github.com>
2020-11-11 13:04:48 -0600

    Create ProctorioExamEndCases.js

---
+++ Proctorio/ProctorioExamEndCases.js
@@ -0,0 +1,39 @@
+"A7220":{"message":"The attempt ended when the test taker disconnected from Proctorio for 2 minutes. <a href='https://proctorio.zendesk.com/hc/articles/204295348' target='_blank' aria-label='The attempt ended when the test taker disconnected from Proctorio for 2 minutes.'>$learn_more$</a>.","description":"lms results performance close code0","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A7221":{"message":"The attempt ended when the test taker submitted the exam. <a href='https://proctorio.zendesk.com/hc/articles/204993777' target='_blank' aria-label='The attempt ended when the test taker submitted the exam.'>$learn_more$</a>.","description":"lms results performance close code1","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A72210":{"message":"The attempt ended when a proctor removed the test taker from their exam","description":"lms results performance close code10"},"A72211":{"message":"The attempt ended when the time limit was exceeded and the exam was auto-submitted. <a href='https://proctorio.zendesk.com/hc/articles/204296438' target='_blank' aria-label='The attempt ended when the time limit was exceeded and the exam was auto-submitted.'>$learn_more$</a>.","description":"lms results performance close code11","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A72212":{"message":"The attempt ended when the test taker attempted to modify the quiz page. <a href='https://proctorio.zendesk.com/hc/articles/204993877' target='_blank' aria-label='The attempt ended when the test taker attempted to modify the quiz page.'>$learn_more$</a>.","description":"lms results performance close code12","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A72213":{"message":"The attempt ended when the test taker attempted to download a file during the quiz. <a href='https://proctorio.zendesk.com/hc/articles/205641867' target='_blank' aria-label='The attempt ended when the test taker attempted to download a file during the quiz.'>$learn_more$</a>.","description":"lms results performance close code13","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A72214":{"message":"The attempt ended when the test taker's battery died. <a href='https://proctorio.zendesk.com/hc/articles/205728638' target='_blank' aria-label='The attempt ended when the test taker's battery died.'>$learn_more$</a>.","description":"lms results performance close code14","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A72215":{"message":"The attempt ended when the test taker plugged in an additional monitor. <a href='https://proctorio.zendesk.com/hc/articles/206456887' target='_blank' aria-label='The attempt ended when the test taker plugged in an additional monitor.'>$learn_more$</a>.","description":"lms results performance close code15","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A72216":{"message":"The attempt ended when the test taker unplugged a camera or microphone. <a href='https://proctorio.zendesk.com/hc/articles/206456897' target='_blank' aria-label='The attempt ended when the test taker unplugged a camera or microphone.'>$learn_more$</a>.","description":"lms results performance close code16","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A72217":{"message":"The attempt ended when the test taker plugged in an additional camera or microphone. <a href='https://proctorio.zendesk.com/hc/articles/206456907' target='_blank' aria-label='The attempt ended when the test taker plugged in an additional camera or microphone.'>$learn_more$</a>.","description":"lms results performance close code17","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A72218":{"message":"The attempt ended when a hardware malfunction occured during the exam <a href='https://proctorio.zendesk.com/hc/articles/206456907' target='_blank' aria-label='The attempt ended when the test taker plugged in an additional camera or microphone.'>$learn_more$</a>.","description":"lms results performance close code18","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A72219":{"message":"The attempt ended when the test takers mobile phone was taken out of lockdown mode. <a href='https://proctorio.zendesk.com/hc/articles/206456907' target='_blank' aria-label='The attempt ended when a the test taker's mobile phone was taken out of lockdown mode.'>$learn_more$</a>.","description":"lms results performance close code19","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A72220":{"message":"The attempt ended when an advanced program detection malfunction occured during the exam. <a href='https://proctorio.zendesk.com/hc/articles/206456907' target='_blank' aria-label='The attempt ended when an advanced program detection malfunction occured during the exam.'>$learn_more$</a>.","description":"lms results performance close code20","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A7222":{"message":"The attempt ended when the test taker navigated away from the exam. <a href='https://proctorio.zendesk.com/hc/articles/204296218' target='_blank' aria-label='The attempt ended when the test taker navigated away from the exam.'>$learn_more$</a>.","description":"lms results performance close code2","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A7223":{"message":"The attempt ended when the test taker reached the exam time limit. <a href='https://proctorio.zendesk.com/hc/articles/204993787' target='_blank' aria-label='https://proctorio.zendesk.com/hc/articles/204993787'>$learn_more$</a>.","description":"lms results performance close code3","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A7224":{"message":"The attempt ended when the test taker left exam when in full screen. <a href='https://proctorio.zendesk.com/hc/articles/204993547' target='_blank' aria-label='The attempt ended when the test taker left exam when in full screen.'>$learn_more$</a>.","description":"lms results performance close code4","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A7225":{"message":"The attempt ended when the test taker ended screen recording. <a href='https://proctorio.zendesk.com/hc/articles/204295948' target='_blank' aria-label='The attempt ended when the test taker ended screen recording.'>$learn_more$</a>.","description":"lms results performance close code5","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A7226":{"message":"The attempt ended when the test taker uninstalled Chrome Extension. <a href='https://proctorio.zendesk.com/hc/articles/204993677' target='_blank' aria-label='The attempt ended when the test taker uninstalled Chrome Extension.'>$learn_more$</a>.","description":"lms results performance close code6","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A7227":{"message":"The attempt ended when the test taker switched to a proxy during the exam. <a href='https://proctorio.zendesk.com/hc/articles/204296228' target='_blank' aria-label='The attempt ended when the test taker switched to a proxy during the exam.'>$learn_more$</a>.","description":"lms results performance close code7","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A7228":{"message":"The attempt ended when the test taker changed networks during the exam. <a href='https://proctorio.zendesk.com/hc/articles/204296328' target='_blank' aria-label='The attempt ended when the test taker changed networks during the exam.'>$learn_more$</a>.","description":"lms results performance close code8","placeholders":{"learn_more":{"content":"$1"}}},
+
+"A7229":{"message":"The attempt ended when the test taker closed or reloaded the exam tab. <a href='https://proctorio.zendesk.com/hc/articles/204296428' target='_blank' aria-label='The attempt ended when the test taker closed or reloaded the exam tab.'>$learn_more$</a>.","description":"lms results performance close code9","placeholders":{"learn_more":{"content":"$1"}}},
