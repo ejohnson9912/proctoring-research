f97177dd46049c97e35803c50bea56425cd5d615
ejohnson9912 <39468634+ejohnson9912@users.noreply.github.com>
2020-11-11 11:26:55 -0600

    Proctorio Metrics/Comparison Snippets
    These files are shortened snippets from Proctorio's Google Chrome extension, displaying algorhithm metrics, banned programs, and comparison metrics. This is strictly for criticism and educational purposes.

---
+++ Proctorio/ProctorioAlgorithmMetrics.js
@@ -0,0 +1,310 @@
+      "description": "lms metrics abnormal audio desc",
+      "message": "e.g. consistently talking or in noisy room"
+   
+   
+      "description": "lms metrics abnormal audio title",
+      "message": "Abnormal changes in audio levels highlights test takers that had significant changes in audio activity throughout the exam."
+   
+   
+      "description": "lms metrics abnormal clicks desc",
+      "message": "e.g. rapidly clicking on various parts of the quiz"
+   
+   
+      "description": "lms metrics abnormal clicks title",
+      "message": "Abnormal clicking will highlight test takers who interacted with the quiz page less than the rest of the class"
+   
+   
+      "description": "lms metrics abnormal copy desc",
+      "message": "e.g. excessive copies and pastes"
+   
+   
+      "description": "lms metrics abnormal copy title",
+      "message": "Abnormal copies and pastes highlights test takers who may have taken material from the exam or brought answers into the exam extensively."
+   
+   
+      "description": "lms metrics abnormal duration desc",
+      "message": "may indicate help from outside sources"
+   
+   
+      "description": "lms metrics abnormal duration title",
+      "message": "Abnormal exam duration will highlight test takers who have finished significantly faster or slower than the rest of the class."
+   
+   
+      "description": "lms metrics abnormal end desc",
+      "message": "may indicate an attempt to bypass system or connectivity issue"
+   
+   
+      "description": "lms metrics abnormal end title",
+      "message": "End Times will highlight test takers whose LMS end time does not match the Proctorio end time"
+   
+   
+      "description": "lms metrics abnormal eyes desc",
+      "message": "e.g. consistently looking at different areas of the screen"
+   
+   
+      "description": "lms metrics abnormal eyes title",
+      "message": "Abnormal eye movement will highlight test takers who may have unauthorized materials within the test environment."
+   
+   
+      "description": "lms metrics abnormal faces desc",
+      "message": "e.g. utilizing additional people"
+   
+   
+      "description": "lms metrics abnormal faces title",
+      "message": "Abnormal number of faces detected will highlight test takers who may have received help from others during the exam."
+   
+   
+      "description": "lms metrics abnormal head desc",
+      "message": "e.g. consistently looking at notes or off the screen"
+   
+   
+      "description": "lms metrics abnormal head title",
+      "message": "Abnormal head movement will highlight test takers who looked away from the camera significantly more or less than the other test takers."
+   
+   
+      "description": "lms metrics abnormal ip desc",
+      "message": "e.g. collaborating with other test takers on quiz at same time"
+   
+   
+      "description": "lms metrics abnormal ip title",
+      "message": "Exam Collusion will highlight test takers who took the exam at the same time on the same network"
+   
+   
+      "description": "lms metrics abnormal keys desc",
+      "message": "e.g. typing more than other test takers"
+   
+   
+      "description": "lms metrics abnormal keys title",
+      "message": "Abnormal use of keystrokes highlights test takers relying on copy and paste or struggling with free response questions."
+   
+   
+      "description": "lms metrics abnormal less",
+      "message": "less"
+   
+   
+      "description": "lms metrics abnormal more",
+      "message": "more"
+   
+   
+      "description": "lms metrics abnormal mouse desc",
+      "message": "e.g. consistently moving the mouse"
+   
+   
+      "description": "lms metrics abnormal mouse title",
+      "message": "Abnormal movement of the mouse will highlight test takers who interacted with the quiz page less than the rest of the class."
+   
+   
+      "description": "lms metrics abnormal resize desc",
+      "message": "e.g. consistently changing quiz window size"
+   
+   
+      "description": "lms metrics abnormal resize title",
+      "message": "Abnormal resizing of the browser highlights test takers who may have had notes or other material hidden behind the quiz window."
+   
+   
+      "description": "lms metrics abnormal scroll desc",
+      "message": "e.g. rapidly moving through the quiz"
+   
+   
+      "description": "lms metrics abnormal scroll title",
+      "message": "Abnormal scrolling will highlight test takers who interacted with the quiz page less than the rest of the class"
+   
+   
+      "description": "lms metrics abnormal start desc",
+      "message": "may indicate an attempt to bypass system or connectivity issue"
+   
+   
+      "description": "lms metrics abnormal start title",
+      "message": "Start Times will highlight test takers whose LMS start time does not match the Proctorio start time"
+   
+   
+      "description": "lms metrics abnormal unfocus desc",
+      "message": "e.g. open separate webpage"
+   
+   
+      "description": "lms metrics abnormal unfocus title",
+      "message": "Abnormal amounts of navigation away highlights test takers using external applications or material differently than the rest of the class."
+   
+   
+      "description": "lms metrics frame audio desc",
+      "message": "e.g. talking or noisy environment"
+   
+   
+      "description": "lms metrics frame audio incident",
+      "message": "Audio levels in the room increased above the threshold"
+   
+   
+      "description": "lms metrics frame audio short",
+      "message": "Audio Levels"
+   
+   
+      "description": "lms metrics frame audio title",
+      "message": "Flag Changes in Audio Levels"
+   
+   
+      "description": "lms metrics frame clicking short",
+      "message": "Clicking"
+   
+   
+      "description": "lms metrics frame collusion short",
+      "message": "Exam Collusion"
+   
+   
+      "description": "lms metrics frame copy desc",
+      "message": "e.g. duplicating exam content"
+   
+   
+      "description": "lms metrics frame copy incident off",
+      "message": "Test taker copied and pasted values"
+   
+   
+      "description": "lms metrics frame copy incident on",
+      "message": "Test taker attempted to copy and paste values"
+   
+   
+      "description": "lms metrics frame copy short",
+      "message": "Copy & Paste"
+   
+   
+      "description": "lms metrics frame copy title",
+      "message": "Flag Copy and Paste"
+   
+   
+      "description": "lms metrics frame duration short",
+      "message": "Exam Duration"
+   
+   
+      "description": "lms metrics frame end time short",
+      "message": "End Times"
+   
+   
+      "description": "lms metrics frame eyes desc",
+      "message": "e.g. look at different monitor"
+   
+   
+      "description": "lms metrics frame eyes incident",
+      "message": "Test taker's eyes were looking away from the quiz page"
+   
+   
+      "description": "lms metrics frame eyes short",
+      "message": "Eye Movement"
+   
+   
+      "description": "lms metrics frame eyes title",
+      "message": "Flag Eye Movements"
+   
+   
+      "description": "lms metrics frame faces desc",
+      "message": "e.g. multiple test takers taking the same exam"
+   
+   
+      "description": "lms metrics frame faces incident",
+      "message": "Multiple faces detected in the image"
+   
+   
+      "description": "lms metrics frame faces short",
+      "message": "Multi-Face"
+   
+   
+      "description": "lms metrics frame faces title",
+      "message": "Flag Multiple Faces"
+   
+   
+      "description": "lms metrics frame head desc",
+      "message": "e.g. look at notes"
+   
+   
+      "description": "lms metrics frame head incident",
+      "message": "Test taker was looking away from the quiz page"
+   
+   
+      "description": "lms metrics frame head short",
+      "message": "Head and Eye Movement"
+   
+   
+      "description": "lms metrics frame head title",
+      "message": "Flag Head and Eye Movements"
+   
+   
+      "description": "lms metrics frame keys desc",
+      "message": "e.g. typing on multiple choice exam"
+   
+   
+      "description": "lms metrics frame keys incident",
+      "message": "Test taker typed on the keyboard"
+   
+   
+      "description": "lms metrics frame keys short",
+      "message": "Keystrokes"
+   
+   
+      "description": "lms metrics frame keys title",
+      "message": "Flag Keystrokes"
+   
+   
+      "description": "lms metrics frame mouse short",
+      "message": "Mouse Movement"
+   
+   
+      "description": "lms metrics frame resize desc",
+      "message": "e.g. move browser to uncover other window"
+   
+   
+      "description": "lms metrics frame resize incident off",
+      "message": "Test taker changed zoom level or re-sized the browser"
+   
+   
+      "description": "lms metrics frame resize incident on",
+      "message": "Test taker attempted to change zoom level or re-size the browser"
+   
+   
+      "description": "lms metrics frame resize short",
+      "message": "Browser Resize"
+   
+   
+      "description": "lms metrics frame resize title",
+      "message": "Flag Browser Resize"
+   
+   
+      "description": "lms metrics frame scrolling short",
+      "message": "Scrolling"
+   
+   
+      "description": "lms metrics frame start time short",
+      "message": "Start Times"
+   
+   
+      "description": "lms metrics frame unfocus desc",
+      "message": "e.g. open separate webpage"
+   
+   
+      "description": "lms metrics frame unfocus incident off",
+      "message": "Test taker navigated away from the quiz page"
+   
+   
+      "description": "lms metrics frame unfocus incident on",
+      "message": "Test taker attempted to navigate away from the quiz page"
+   
+   
+      "description": "lms metrics frame unfocus short",
+      "message": "Navigating Away"
+   
+   
+      "description": "lms metrics frame unfocus title",
+      "message": "Flag New Windows or Tabs"
+   
+   
+      "description": "lms metrics tamper av desc",
+      "message": "Potential audio/video hardware issue detected"
+   
+   
+      "description": "lms metrics tamper av title",
+      "message": "Video Tamper"
+   
+   
+      "description": "lms metrics tamper page desc",
+      "message": "Potential webpage issue detected"
+   
+   
+      "description": "lms metrics tamper page title",
+      "message": "Page Tamper"
---
+++ Proctorio/ProctorioComparisonMetrics.js
@@ -0,0 +1,165 @@
+      "description": "copy/paste",
+      "message": "Test taker $action$ values $val$% $rel$ than the average.",
+      "placeholders": {
+         "action": {
+            "content": "$1",
+            "description": "the test taker's action based on what the professor enabled"
+         
+         "rel": {
+            "content": "$3",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$2",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   
+      "description": "resize",
+      "message": "Test taker $action$ zoom level or browser size $val$% $rel$ than the average.",
+      "placeholders": {
+         "action": {
+            "content": "$1",
+            "description": "the action the test taker performed based on the professor settings"
+         
+         "rel": {
+            "content": "$3",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$2",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   
+      "description": "audio",
+      "message": "Audio levels exceeded the threshold $val$% $rel$ than the average.",
+      "placeholders": {
+         "rel": {
+            "content": "$2",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$1",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   
+      "description": "head movement",
+      "message": "Test taker was looking away from the quiz page $val$% $rel$ than the average.",
+      "placeholders": {
+         "rel": {
+            "content": "$2",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$1",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   
+      "description": "eye movement",
+      "message": "Test taker was looking away from the quiz page $val$% $rel$ than the average.",
+      "placeholders": {
+         "rel": {
+            "content": "$2",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$1",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   
+      "description": "multi face",
+      "message": "Multiple faces were detected in the image $val$% $rel$ than the average.",
+      "placeholders": {
+         "rel": {
+            "content": "$2",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$1",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   
+      "description": "mouse movement",
+      "message": "Test taker moved the mouse $val$% $rel$ than the average.",
+      "placeholders": {
+         "rel": {
+            "content": "$2",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$1",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   
+      "description": "mouse movement",
+      "message": "Test taker scrolled $val$% $rel$ than the average.",
+      "placeholders": {
+         "rel": {
+            "content": "$2",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$1",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   
+      "description": "mouse movement",
+      "message": "Test taker clicked the mouse $val$% $rel$ than the average.",
+      "placeholders": {
+         "rel": {
+            "content": "$2",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$1",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   
+      "description": "mouse movement",
+      "message": "Test taker took $val$% $rel$ time than the average to complete the exam.",
+      "placeholders": {
+         "rel": {
+            "content": "$2",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$1",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   "A695_8": {
+      "description": "navigation away from the page",
+      "message": "Test taker $action$ from the quiz page $val$% $rel$ than the average.",
+      "placeholders": {
+         "action": {
+            "content": "$1",
+            "description": "the action that was performed (based on prof settings)"
+         
+         "rel": {
+            "content": "$3",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$2",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   "A695_9": {
+      "description": "keystrokes",
+      "message": "Test taker used the keyboard $val$% $rel$ than the average.",
+      "placeholders": {
+         "rel": {
+            "content": "$2",
+            "description": "more/less based on the value"
+         
+         "val": {
+            "content": "$1",
+            "description": "the test taker's value relative to the average (%)"
+ 
+   
+      "description": "lms results metrics summary text ip",
+      "message": "$count$ other test taker$plural$ took the exam on the same connection at the same time.",
+      "placeholders": {
+         
+            "content": "$1"
+         
+         "plural": {
+            "content": "$2"
+ 
